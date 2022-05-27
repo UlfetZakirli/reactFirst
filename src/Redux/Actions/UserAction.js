a0415318c9cae743b18deba00f453bd651beba8c
UlfetZakirli <ulfetzakirli@gmail.com>
2022-05-27 14:59:04 +0400

    dhhd

--- src/Redux/Actions/UserAction.js
+++ src/Redux/Actions/UserAction.js
@@ -3,6 +3,7 @@ import { USER_LOGIN_SUCCESS } from "../Constants/UserConstant";
 
 import { BASE_URL } from './../../api/ApiConfig';
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST } from './../Constants/UserConstant';
+
 export const loginAction=(email,password)=>(dispatch,getState)=>{
 
     try{
@@ -24,15 +25,19 @@ const config={
 
 
 
+export const registerAction=(firstName,lastName,email,password,confirmPassword)=>(dispatch,getState)=>{
 
-export const registerAction=(firstName,lastName,email,password,confirmPassword)=>(distpacth,getSatet)=>{
     try{
-     
-
-    }catch(error){
-    
+        dispatch({type:USER_LOGIN_REQUEST})
+const config={
+    headers:{
+        "Content-Type":"application.json"
     }
 }
-export const logoutAction=(email,password)=>(dispatch,getState)=>{
-
+        const {data}=axios.get(`${BASE_URL}/api/account/login`,{email,password},config);
+        dispatch({type:USER_LOGIN_SUCCESS,payload:data})
+        localStorage.setItem("userItem",JSON.stringify(data))
+    }catch(error){
+        dispatch({type:USER_LOGIN_FAIL,payload:error})
+    }
 }
